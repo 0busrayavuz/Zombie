212a727a77a47499e0fc485d42854b76bb5ef7c0
Busra Yavuz <busrayavuzz77@icloud.com>
2026-06-09 10:34:13 +0300

    feat: add victory music on player win

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     static bool hasPlayerWon;
+    static GameManager instance;
 
     [SerializeField] float secondsToLand = 180f;
     [SerializeField] bool winWhenTimerExpiresWithoutHelicopter = false;
     [SerializeField] Helicopter helicopter;
 
     Health playerHealth;
+    AudioSource victoryAudio;
     float landingTime;
 
     public static event Action PlayerWon;
@@ -25,8 +27,28 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
+        instance = this;
         hasPlayerWon = false;
         RescueHasArrived = false;
+
+        AudioClip victoryClip = Resources.Load<AudioClip>("Audio/VictoryMusic");
+        if (victoryClip != null)
+        {
+            victoryAudio = gameObject.AddComponent<AudioSource>();
+            victoryAudio.clip = victoryClip;
+            victoryAudio.playOnAwake = false;
+            victoryAudio.loop = false;
+            victoryAudio.spatialBlend = 0f;
+            victoryAudio.volume = 0.8f;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
     }
 
     void Start()
@@ -87,6 +109,27 @@ public class GameManager : MonoBehaviour
         hasPlayerWon = true;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        if (instance != null)
+        {
+            instance.PlayVictoryAudio();
+        }
+
         PlayerWon?.Invoke();
+    }
+
+    void PlayVictoryAudio()
+    {
+        foreach (AudioSource source in FindObjectsByType<AudioSource>(FindObjectsSortMode.None))
+        {
+            if (source != victoryAudio && source.loop)
+            {
+                source.Stop();
+            }
+        }
+
+        if (victoryAudio != null)
+        {
+            victoryAudio.Play();
+        }
     }
 }
